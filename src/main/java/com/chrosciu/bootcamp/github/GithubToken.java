6dda3fe9cfa21032240b825dd613708a26b7a794
Marcin Chrost <marcinc@backbase.com>
2022-03-18 10:19:56 +0100

    github token stored in properties

--- src/main/java/com/chrosciu/bootcamp/github/GithubToken.java
+++ src/main/java/com/chrosciu/bootcamp/github/GithubToken.java
@@ -1,5 +1,27 @@
 package com.chrosciu.bootcamp.github;
 
+import lombok.SneakyThrows;
+
+import java.io.InputStream;
+import java.util.Properties;
+
 public class GithubToken {
-    public static final String TOKEN = "";
+    private static final Properties properties;
+    public static final String TOKEN;
+
+    static {
+        properties = new Properties();
+        loadProperties();
+        TOKEN = properties.getProperty("github.token");
+        if (null == TOKEN) {
+            throw new RuntimeException("Cannot read github token");
+        }
+    }
+
+    @SneakyThrows
+    private static void loadProperties() {
+        try (final InputStream stream = GithubToken.class.getResourceAsStream("/github.properties")) {
+            properties.load(stream);
+        }
+    }
 }
